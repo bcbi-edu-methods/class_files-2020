8db63760c199b71dc6dd516e1e08fb245a137be8
Neil Sarkar <neil_sarkar@brown.edu>
2020-02-27 11:54:43 -0500

    Update methods2020_program3.jl

--- code/methods2020_program3.jl
+++ code/methods2020_program3.jl
@@ -1,5 +1,6 @@
 # program 3: arrays, sets, and dictionaries-- oh my!
 
+#=
 # ask user for a phrase
 println("enter phrase")
 phrase = readline(stdin)
@@ -45,6 +46,8 @@ for word in word_array
 
 end
 
+=#
+
 ##### BEGIN CLASS EXERCISE
 ## Until the user types "stop,stop", ask the user for translations as key, value
 ## pairs that will be used to convert phrases later. Then:
@@ -52,6 +55,51 @@ end
 ##		(2) Reports the unique list of keys
 ##		(3) Asks the user for 3 words to translate, and reports the translation
 
+key_val_pair = ""
+translate_dict = Dict()
+key_array = []
+key_set = Set()
+
+print("enter a translation (as \"key,value\"): ")
+key_val_pair = readline(stdin)
+
+while key_val_pair != "stop,stop"
+
+	key_val_array = split(key_val_pair, ",")
+
+	key = key_val_array[1]
+	val = key_val_array[2]
+
+	push!(key_array, key)
+	push!(key_set, key)
+
+	translate_dict[key] = val
+	print("enter a translation (as \"key,value\"): ")
+	global key_val_pair = readline(stdin)
+end
+
+println("thanks. The order that keys were entered was:")
+for key in key_array
+	println(key)
+end
+
+println("\nwhich consisted of the following unique keys:")
+for key in key_set
+	println(key)
+end
+
+for word_count in 1:3
+	print("give me a word/phrase to translate ($word_count): ")
+	translate_phrase = readline(stdin)
+
+	if haskey(translate_dict, translate_phrase)
+		translation = translate_dict[translate_phrase]
+		println("that translates to: $translation")
+	else
+		println("no translation available")
+	end
+
+end
 
 
 
